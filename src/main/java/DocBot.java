59a2bff4201d6cac19528e8b334e71b246890193
svakulenko <serhii.vakulenko@ukma.edu.ua>
2020-09-19 13:53:35 +0300

    check

--- src/main/java/DocBot.java
+++ src/main/java/DocBot.java
@@ -1,7 +1,14 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class DocBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
@@ -22,19 +29,42 @@ public class DocBot extends TelegramLongPollingBot {
 
     }
 
-   public String getResponsesArray(String messageFromTheCustomer){
 
-        if(messageFromTheCustomer.equals("/start")){
-            return "Твій Бот-Лікар на місці!";
+   public String getResponsesArray(String messageFromTheCustomer) {
+
+
+        try{
+
+            Reader reader = Files.newBufferedReader(Paths.get("customer.json"));
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            JsonNode parser = objectMapper.readTree(reader);
+
+            System.out.println(parser.path("id").asLong());
+            System.out.println(parser.path("name").asText());
+            System.out.println(parser.path("email").asText());
+            System.out.println(parser.path("age").asLong());
+
+
+        }catch (Exception ex) {
+            ex.printStackTrace();
         }
-        else if(messageFromTheCustomer.contains("Привіт")){
-            return "Привіт, що тебе хвилює?";
-        }
-        else if(messageFromTheCustomer.contains("болить")){
-            return "Опиши свій біль?";
-        }
-        return "Гарного здоров‘ячка ;)";
+
+
+//       if(messageFromTheCustomer.equals("/start")){
+//            return "Твій Бот-Лікар на місці!";
+//        }
+//        else if(messageFromTheCustomer.contains("Привіт")){
+//            return "Привіт, що тебе хвилює?";
+//        }
+//        else if(messageFromTheCustomer.contains("болить")){
+//            return "Опиши свій біль?";
+//        }
+
+         return "Гарного здоров‘ячка ;)";
    }
+
+
 
     public String getBotUsername() {
         return "docHelpNaUKMA_bot";
